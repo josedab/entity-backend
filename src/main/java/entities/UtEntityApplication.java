1bdea1f477f9c3d6e488740d30c57791b13d8324
Jose David Baena <josedab@gmail.com>
2015-04-05 23:58:31 +0100

    Initial setup for entity microservice - Spring boot setup

---
+++ src/main/java/entities/UtEntityApplication.java
@@ -0,0 +1,12 @@
+package entities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtEntityApplication.class, args);
+    }
+}
---
+++ src/test/java/entities/UtEntityApplicationTests.java
@@ -0,0 +1,16 @@
+package entities;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = UtEntityApplication.class)
+public class UtEntityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
